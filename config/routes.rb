e8d43d9207816f57b6d0d8c901a61c711bf0ed79
Justin Streufert <monitron@gmail.com>
2016-11-12 22:44:01 -0500

    root route

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   end
   #resources :concerns
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/', to: 'ideas#random'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161113004115) do
+ActiveRecord::Schema.define(version: 20161113033407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20161113004115) do
     t.integer  "click_count",       default: 0,     null: false
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
+    t.index ["slug"], name: "index_ideas_on_slug", using: :btree
   end
 
 end
