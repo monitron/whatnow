cfd8b6f4c0d082093702d8b1a0a1e46606cf9e4c
Justin Streufert <monitron@gmail.com>
2016-11-15 01:05:17 -0500

    Add Idea form

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -3,11 +3,8 @@ class IdeasController < ApplicationController
     unless session[:current_concern].blank?
       @concern = Concern.find(session[:current_concern])
     end
-    if @concern.blank?
-      @idea = Idea.order('random()').first
-    else
-      @idea = @concern.ideas.order('random()').first
-    end
+    ideas = @concern.blank? ? Idea : @concern.ideas
+    @idea = ideas.where(approved: true).order('random()').first
     redirect_to @idea
   end
 
@@ -19,5 +16,19 @@ class IdeasController < ApplicationController
     @idea = Idea.find_by_slug(params[:id])
     @social_description = @idea.name
     @one_screen = true
+  end
+
+  def new
+    @idea = Idea.new
+    @concerns = Concern.all
+  end
+
+  def create
+    @idea = Idea.new(params.require(:idea).permit(:concern_id, :name,
+      :action_url, :description, :contributor_name, :contributor_place))
+    unless @idea.save
+      @concerns = Concern.all
+      render "new"
+    end
   end
 end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -3,6 +3,13 @@ class Idea < ApplicationRecord
 
   before_create :generate_slug
 
+  validates :name, :action_url, :description, presence: true
+  validates :name, length: {maximum: 60}
+  validates :action_url, length: {maximum: 250}
+  validates :description, length: {maximum: 200}, allow_blank: true
+  validates :contributor_name, length: {maximum: 40}, allow_blank: true
+  validates :contributor_place, length: {maximum: 30}, allow_blank: true
+
   def generate_slug
     self.slug = SecureRandom.hex(10)
   end
